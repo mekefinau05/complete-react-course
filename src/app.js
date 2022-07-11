a7db944dbba9cd1cdfedd01598a401c19f304c34
Meke Finau <lonifinau.09@gmail.com>
2022-07-11 08:34:57 -0600

    pg

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 console.log('App dot js is running');
 
 // JSX
-let template = <h1>Indecision App</h1>;
+let template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+  </div>
+);
 let appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
