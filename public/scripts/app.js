cd784086c148499755cd2a51de88c102a6fcfcba
Meke Finau <lonifinau.09@gmail.com>
2022-07-11 12:10:16 -0600

    ternary

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,79 @@
 console.log('App dot js is running');
 
 // JSX
+
+var temp = {
+  title: 'Indecision App',
+  subTitle: 'This is the subtitle for the Indecision App course'
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    temp.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    temp.subTitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'Sione Finau',
+  age: 5,
+  location: 'Utah'
+};
+
+var username = 'Meke Finau';
+var userAge = 30;
+var userLocation = 'Saratoga Springs';
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  ' ',
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
 console.log('App dot js is running');
 
 // JSX
+
+let temp = {
+  title: 'Indecision App',
+  subTitle: 'This is the subtitle for the Indecision App course',
+};
+
 let template = (
   <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
+    <h1>{temp.title}</h1>
+    <p>{temp.subTitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
   </div>
 );
+
+let user = {
+  name: 'Sione Finau',
+  age: 5,
+  location: 'Utah',
+};
+
+let username = 'Meke Finau';
+let userAge = 30;
+let userLocation = 'Saratoga Springs';
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+let templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>{' '}
+    {/* TERNARY - great for conditions on one of the two things*/}
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {/* && - great for conditions on one thing */}
+    {getLocation(user.location)}
+    {/* ELSE - nothing at all */}
+  </div>
+);
+
 let appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
